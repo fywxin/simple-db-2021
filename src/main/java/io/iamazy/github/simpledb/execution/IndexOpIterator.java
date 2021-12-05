01d43e0847e7be59edf391c4a7181ab60ef5b3df
iamazy <1448588084@qq.com>
2021-12-05 12:39:01 +0800

    Initial commit

---
+++ src/main/java/io/iamazy/github/simpledb/execution/IndexOpIterator.java
@@ -0,0 +1,23 @@
+package io.iamazy.github.simpledb.execution;
+import io.iamazy.github.simpledb.transaction.TransactionAbortedException;
+import io.iamazy.github.simpledb.common.DbException;
+
+import java.util.*;
+
+/** IndexDBIterator is the interface that index access methods
+    implement in SimpleDb.
+*/
+public interface IndexOpIterator extends OpIterator {
+    /** Open the access method such that when getNext is called, it
+        iterates through the tuples that satisfy ipred.
+        @param ipred The predicate that is used to scan the index.
+    */
+    void open(IndexPredicate ipred)
+        throws NoSuchElementException, DbException, TransactionAbortedException;
+
+    /** Begin a new index scan with the specified predicate.
+        @param ipred The predicate that is used to scan the index.
+    */
+    void rewind(IndexPredicate ipred)
+        throws DbException, TransactionAbortedException;
+}
