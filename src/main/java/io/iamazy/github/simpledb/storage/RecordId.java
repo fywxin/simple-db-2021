82135b4d070c471ea407e7822726dae9b9b287bb
iamazy <1448588084@qq.com>
2021-12-07 00:21:19 +0800

    add lab1 - HeapFile access method

--- src/main/java/io/iamazy/github/simpledb/storage/RecordId.java
+++ src/main/java/io/iamazy/github/simpledb/storage/RecordId.java
@@ -1,6 +1,7 @@
 package io.iamazy.github.simpledb.storage;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * A RecordId is a reference to a specific tuple on a specific page of a
@@ -9,6 +10,9 @@ import java.io.Serializable;
 public class RecordId implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    private final PageId pageId;
+    private final int tupleNum;
 
     /**
      * Creates a new RecordId referring to the specified PageId and tuple
@@ -21,6 +25,8 @@ public class RecordId implements Serializable {
      */
     public RecordId(PageId pid, int tupleno) {
         // some code goes here
+        this.pageId = pid;
+        this.tupleNum = tupleno;
     }
 
     /**
@@ -28,7 +34,7 @@ public class RecordId implements Serializable {
      */
     public int getTupleNumber() {
         // some code goes here
-        return 0;
+        return tupleNum;
     }
 
     /**
@@ -36,7 +42,7 @@ public class RecordId implements Serializable {
      */
     public PageId getPageId() {
         // some code goes here
-        return null;
+        return pageId;
     }
 
     /**
@@ -48,7 +54,10 @@ public class RecordId implements Serializable {
     @Override
     public boolean equals(Object o) {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RecordId recordId = (RecordId) o;
+        return tupleNum == recordId.tupleNum && pageId.equals(recordId.pageId);
     }
 
     /**
@@ -60,8 +69,6 @@ public class RecordId implements Serializable {
     @Override
     public int hashCode() {
         // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
+        return Objects.hash(pageId, tupleNum);
     }
-
 }
